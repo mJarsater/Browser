8c989fa8b5f8aac5de54092c8f2173e0b08a645b
mjarsater <martin.jarsater@hotmail.com>
2020-10-08 10:34:33 +0200

    Allt funkar, kika på css-extension

--- src/Browser.java
+++ src/Browser.java
@@ -1,8 +1,15 @@
 import javax.swing.*;
 import javax.swing.event.HyperlinkEvent;
 import javax.swing.event.HyperlinkListener;
+import javax.swing.text.Document;
+import javax.swing.text.html.HTMLEditorKit;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Browser extends JFrame {
     private JEditorPane browserWindow;
@@ -13,7 +20,6 @@ public class Browser extends JFrame {
         super("Simple Web Browser");
         // --------------- URL FIELD ----------------------
         addressField = new JTextField("Enter a URL..");
-        addressField.addActionListener(this:: searchListner);
         add(addressField, BorderLayout.NORTH);
         // --------------- URL FIELD END ------------------
 
@@ -21,14 +27,11 @@ public class Browser extends JFrame {
 
         browserWindow = new JEditorPane();
         browserWindow.setEditable(false);
-        browserWindow.addHyperlinkListener(new HyperlinkListener() {
-            @Override
-            public void hyperlinkUpdate(HyperlinkEvent event) {
-                if (event.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
-                loadSite(event.getURL().toString());
-            }
-            }
-        });
+        browserWindow.setContentType("text/html");
+
+
+
+        addressField.addActionListener(this:: browserListner);
         add(new JScrollPane(browserWindow), BorderLayout.CENTER);
 
         // --------------- WINDOW END ---------------------
@@ -37,22 +40,38 @@ public class Browser extends JFrame {
         this.setSize(700, 500);
 
     }
+    public void browserListner(ActionEvent event){
+        try {
 
-    public void searchListner(ActionEvent actionEvent){
-        loadSite(actionEvent.getActionCommand());
-    }
+            clearWindow();
+            StringBuffer buffer = new StringBuffer();
+            String line = "";
 
-    private void loadSite(String text){
-        try{
-            browserWindow.setPage(text);
-            addressField.setText(text);
-        } catch (Exception e){
+            URL url = new URL(addressField.getText());
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openStream()));
+            while((line = bufferedReader.readLine()) != null){
+                buffer.append(line+"\n");
+            }
+            displayText(buffer.toString());
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    public void clearWindow() { browserWindow.setText(""); }
+
+    public void displayText(String text){ browserWindow.setText(text); }
 
     public static void main(String[] args) {
         new Browser();
     }
 }
+ /*
+ * HTMLEditorKit htmlKit = new HTMLEditorKit();
+ * browserWindow.setEditorKit(htmlKit);
+ *
+ * */
